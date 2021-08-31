87c030b64acebf964a6327ff6ca219283d1968cb
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-08-31 12:19:48 -0700

    Event Binding

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            frase1: 'Esta es la primera frase',
-            frase2: '<h1>Esta es la segunda frase </h1>',
-            vuelink: 'https://vuejs.org/'
-        }
+            counter: 0,
+        };
     },
-    methods: {
-        outpuGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return this.frase1;
-            } else {
-                return this.frase2;
-            }
-        }
-    }
 });
 
-app.mount('#user-goal');
+app.mount('#events');
