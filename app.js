c346a1a1d38ca8d19af305b4cab4df9c724ee09a
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-08-31 16:59:53 -0700

    Data Binding + Event Binding vs V-model

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 10,
+            counter: 0,
             name: '',
-            confirmName: ''
+            name2: ''
         };
     },
     methods: {
-        confimNameInput() {
-            this.confirmName = this.name;
-        },
-        submitForm() {
-            // 1ra manera event.preventDefault();
-            alert('Submited!')
-        },
-        setName(event, nombre) {
-            this.name = event.target.value + '' + nombre;
+        //Al escribir el valor del input me lo guarda en SetName GUARDA
+        //El v-bind me actualiza el input con el Reset ENVIA
+        setName(event) {
+            //Name le asigne el evento del input
+            //V-model automaticamente asigna el valor a NAME, 
+            this.name = event.target.value;
         },
         add(num) {
             this.counter = this.counter + num;
         },
-        less(num) {
+        reduce(num) {
             this.counter = this.counter - num;
+            // this.counter--;
+        },
+        resetInput() {
+            //Name le asigne espacios
+            //Se puede imprimir directo
+            //v-bind = name hace la union de nuevo valor en input y envia
+            this.name = '';
+        },
+        //Con introducir solo v-model me trae el valor del input 
+        //Y si doy click me lo resetea
+        resetInput2() {
+            //v-model = name hace la union en nuevo valor en input y envia
+            this.name2 = '';
         }
     }
 });
