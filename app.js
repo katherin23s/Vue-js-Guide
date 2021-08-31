ef24c8ce63555c1e6bcb15e07660ecd1e7ebebcf
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-08-31 13:14:05 -0700

    Event Argument

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = Vue.createApp({
         };
     },
     methods: {
-        add() {
-            this.counter = this.counter + 1;
+        add(num) {
+            this.counter = this.counter + num;
         },
-        less() {
-            this.counter = this.counter - 1;
+        less(num) {
+            this.counter = this.counter - num;
         }
     }
 });
