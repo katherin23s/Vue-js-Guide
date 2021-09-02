a9741334384d8c3b84bb30ec36cb8da370a24697
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-09-02 16:42:03 -0700

    Introducing components

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
             friends: [{
                 id: 'maria',
                 name: 'maria lopez',
@@ -15,13 +14,35 @@ const app = Vue.createApp({
                 email: 'julie@gmail.com'
             }]
         }
-    },
+    }
+})
 
+app.component('friend-contact', {
+    template: `
+    <li v-for="friend in friends" :key="friend.id">
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{friend.phone}}</li>
+      <li><strong>Email:</strong> {{friend.email}}</li>
+    </ul>
+  </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'maria',
+                name: 'maria lopez',
+                phone: '77625374',
+                email: 'maria@gmail.com'
+            }
+        };
+    },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
         }
     }
 })
-
 app.mount('#app');
