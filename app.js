fdcb6988ae8fab0572798ea2dae7382369f631d5
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-09-02 11:05:31 -0700

    Adding a Special Attack

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
-function getRandomValue(min, max){
-    return Math.floor(Math.random()*(max-min))+min;
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
     data() {
-      return { 
-        playerHealth: 100,
-        monsterHealth: 100
-      }
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+            currentRound: 0
+        }
     },
-    computed:{
-        monsterBarStyles(){
-            return {width: this.monsterHealth + '%'}
+    computed: {
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
-        attackMonster(){
-            const attackValue = getRandomValue(5,12);
+        attackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
         },
 
-        attackPlayer(){
-            const attackValue = getRandomValue(8,15);
-            this.playerHealth -=attackValue;
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        },
+
+        specialAttackMonster() {
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
-  });
-  
-  app.mount('#game');
+});
+
+app.mount('#game');
