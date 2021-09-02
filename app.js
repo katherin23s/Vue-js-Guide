0f1d1dc37f47cf6e5c20febecf75a81eaa94aadb
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-09-02 16:08:28 -0700

    Understanding the problem of dont use components

--- app.js
+++ app.js
@@ -1,106 +1,27 @@
-function getRandomValue(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
 const app = Vue.createApp({
     data() {
         return {
-            playerHealth: 100,
-            monsterHealth: 100, //barras
-            currentRound: 0, //bloqueo de boton
-            winner: null, //si aparece cierto elemento y estados
-            logMessages: []
+            detailsAreVisible: false,
+            friends: [{
+                id: 'maria',
+                name: 'maria lopez',
+                phone: '77625374',
+                email: 'maria@gmail.com'
+            },
+            {
+                id: 'julie',
+                name: 'Julie Jones',
+                phone: '77625374',
+                email: 'julie@gmail.com'
+            }]
         }
     },
-    computed: {
-        monsterBarStyles() {
-            if (this.monsterHealth < 0) {
-                return { width: '0%' };
-            }
-            return { width: this.monsterHealth + '%' }
-        },
-        playerBarStyles() {
-            if (this.playerHealth < 0) {
-                return { width: '0%' };
-            }
-            return { width: this.playerHealth + '%' }
-        },
-        mayUseSpecialAttack() {
-            return this.currentRound % 3 !== 0;
-        }
-    },
-    watch: {
-        playerHealth(value) {
-            if (value <= 0 && this.monsterHealth <= 0) {
-                //A draw
-                this.winner = 'draw';
-            } else if (value <= 0) {
-                //Player Lost
-                this.winner = 'monster';
-            }
-        },
-        monsterHealt(value) {
-            if (value <= 0 && this.playerHealth <= 0) {
-                //A draw
-                this.winner = 'draw';
-            } else if (value <= 0) {
-                //Monster lost
-                this.winner = 'player';
-            }
-        }
-    },
+
     methods: {
-        startGame() {
-            this.playerHealth = 100;
-            this.monsterHealth = 100;
-            this.winner = null;
-            this.currentRound = 0;
-            this.logMessages = [];
-        },
-        attackMonster() {
-            this.currentRound++;
-            const attackValue = getRandomValue(5, 12);
-            this.monsterHealth -= attackValue;
-            this.addLogMessages('player', 'attack', attackValue);
-            this.attackPlayer();
-        },
-
-        attackPlayer() {
-            const attackValue = getRandomValue(8, 15);
-            this.playerHealth -= attackValue;
-            this.addLogMessages('monster', 'attack', attackValue)
-        },
-
-        specialAttackMonster() {
-            const attackValue = getRandomValue(10, 25);
-            this.monsterHealth -= attackValue;
-            this.addLogMessages('player', 'special-attack', attackValue);
-            this.attackPlayer();
-        },
-
-        healPlayer() {
-            const healValue = getRandomValue(8, 20)
-            if (this.playerHealth + healValue > 100) {
-                this.playerHealth = 100;
-            } else {
-                this.playerHealth += healValue;
-            }
-            this.addLogMessages('player', 'heal', healValue);
-            this.attackPlayer();
-        },
-        surrender() {
-            this.winner = 'monster';
-        },
-
-        addLogMessages(who, what, value) {
-            //agregamos un objeto a nuestro Log Mensajes
-            this.logMessages.unshift({
-                actionBy: who,
-                actionType: what,
-                actionValue: value
-            })
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
         }
     }
-});
+})
 
-app.mount('#game');
+app.mount('#app');
