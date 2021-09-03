0b4a39e7f7fa1ce6834efa2945fc8f8ec23949a8
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-09-02 18:03:38 -0700

    Understanding why props dont should mutate

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,52 @@
+<template>
+  <li>
+    <h2>{{ name }} {{isFavorite === '1' ? '(Favorite)':''}}</h2>
+    <button @click="toggleFavorite"> Toggle Favorite</button>
+       <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+    props:[
+    'name',
+    'phoneNumber',
+    'emailAddress',
+    'is-favorite'
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+      this.phoneNumber
+    },
+    toggleFavorite(){
+      if(this.isFavorite === '1'){
+        this.isFavorite = '0'
+      }else{
+        this.isFavorite = '1'
+      }
+    }
+  }
+};
+</script>
